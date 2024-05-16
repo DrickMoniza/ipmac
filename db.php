086e38311398cbf02e3d5e8458e0bb08e9ddff35
DrickMoniza <168396531+DrickMoniza@users.noreply.github.com>
2024-05-16 13:33:45 +0800

    Add files via upload

--- db.php
+++ db.php
@@ -1,9 +1,9 @@
 <?php
     //https://databases-auth.000webhost.com/index.php?route=/database/structure&db=id22102970_dbmarine_automation
     $servername='localhost';
-    $username='id22102970_root'; //'root';  //id22102970_root
+    $username='root';  //id22102970_root
     $password='Cg@b092006';
-    $dbname = 'https://databases-auth.000webhost.com/index.php?route=/database/structure&db=id22102970_dbmarine_automation'; //'dbmarine_automation';
+    $dbname = 'dbmarine_automation';
     $conn = mysqli_connect($servername,$username,$password,"$dbname");
     
     if(!$conn){
